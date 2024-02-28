7a79e74e79cce3093bf2df7144aca8fd88cd6190
anthonymanciny <141266961+anthonymanciny@users.noreply.github.com>
2024-02-28 12:53:12 -0400

    .

---
+++ script.js
@@ -0,0 +1,11 @@
+const speed = 4, gravity = 0.6;
+const sonic = document.querySelector('sonic')
+const wall = document.querySelector('wall')
+
+const start = document.querySelector('.start')
+const gameover = document.querySelector('.gameover')
+
+
+const startGame = ()=>{
+    
+}
